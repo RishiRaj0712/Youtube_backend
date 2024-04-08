faaec2871b45ef036b2cd692935602631122111a
RishiRaj0712 <rishiraj.rr01@gmail.com>
2024-04-08 23:44:30 +0530

    added tweet and video controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,5 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose"
 import {Tweet} from "../models/tweet.model.js"
+import {Like} from "../models/like.model.js"
 import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
@@ -7,18 +8,178 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content} = req.body
+
+    if(!content || content.trim()===""){
+        throw new ApiError(400, "Tweet can not be empty")
+    }
+
+    const user = req.user?._id
+
+    const tweet = await Tweet.create({
+        content :content,
+        owner:user
+    })
+
+    const createdTweet = await Tweet.findById(tweet._id)
+
+    if(!createTweet){
+        throw new ApiError(500, "Failed to create tweet")
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200,createdTweet,"Tweet created successfully!"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId} = req.params
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid user id")
+    }
+
+    const user = await User.findById(userId)
+
+    if(!user){
+        throw new ApiError(400, "User not found")
+    }
+
+    const tweet = await Tweet.aggregate([
+        {
+            $match:{
+                owner:mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup:{
+                from:"likes",
+                localField:"_id",
+                foreignField:"tweet",
+                as : "tweetLikedBy"
+            }
+        },
+        {
+            $group:{
+                _id:"$_id",
+                content:{$first:"$content"},
+                owner:{$first:"$owner"},
+                createdAt:{$first:"$createdAt"},
+                updatedAt:{$first:"$updatedAt"},
+                totalLikes :{$sum:{$size:"$tweetLikedBy"}}
+            }
+        },
+        {
+            $sort:{
+                updatedAt:-1
+            }
+        }
+    ])
+
+    if(!tweet?.length){
+        throw new ApiError(400, "No tweet found")
+    }
+
+    const tweetedBy  =  await Tweet.aggregate([
+        {
+            $match:{
+                _id:mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $addFields:{
+                isTweetOwner:{
+                    $cond:{
+                        if:{
+                            $eq:[res.user?._id.toString(),userId]
+                        },
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                username:1,
+                fullName:1,
+                avator:1,
+                createdAt:1,
+                updatedAt:1,
+                isTweetOwner:1
+            }
+        }
+    ])
+
+    const tweetList = {
+        tweet,
+        tweetedBy
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200,tweetList,"User tweets retrived successfully!"))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {tweetId, content} = req.params
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(400, "Tweet not found!")
+    }
+
+    if(!content || content.trim()===""){
+        throw new ApiError(400, "Tweet body cannot be empty")
+    }
+
+    const user = req.user?._id
+    if(tweet.owner.toString()!==user.toString()){
+        throw new ApiError(400, "You are not allowed to update this tweet")
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(tweetId,{
+        content
+    },
+    {
+        new :true
+    })
+    if(!updatedTweet){
+        throw new ApiError(500, "Failed to update tweet")
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200, updatedTweet, "Tweet updated successfully!"))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.params
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet id")
+    }
+    const user = req.user?._id
+    if(!user){
+        throw new ApiError(400, "Unathuorized")
+    }
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(400, "Tweet not found!")
+    }
+    if(tweet.owner.toString()!==user.toString()){
+        throw new ApiError(400, "You are not allowed to delete this tweet!")
+    }
+
+    try {
+        await Tweet.findByIdAndDelete(tweetId)
+        await Like.deleteMany({tweet:tweetId})
+        return res.status(200)
+        .json(new ApiResponse(500, "Tweet deleted succesfully!"))
+    } catch (error) {
+        throw new ApiError(500, "Failed to delete tweet")
+    }
 })
 
 export {
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,33 +8,213 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
+    try {
+        const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+        //TODO: get all videos based on query, sort, pagination
+    
+        const sortTypeNum = Number(sortType) || -1;
+        const pageNum = Number(page)
+        const limitNum = Number(limit)
+    
+        if(userId && !isValidObjectId(userId)){
+            throw new ApiError(400,"Invalid User id")
+        }
+    
+        const getVideos = await Video.aggregate([
+            {
+                $match:{
+                    owner:userId? userId:"",
+                    isPublised : true,
+                    $text:{
+                        $search:query?query:""
+                    }
+                }
+            },
+            {
+                $addFields:{
+                    sortField:{
+                        $toString: `$`+(sortBy||`createdAt`)
+                    }
+                }
+            },
+            {
+                $facet:{
+                    videos:[
+                        {
+                            $sort:{sortField:sortTypeNum}
+                        },
+                        {
+                            $skip:(pageNum-1)*limitNum
+                        },
+                        {
+                            $limit:limitNum
+                        },
+                        {
+                            $lookup:{
+                                from:"users",
+                                localField:"owner",
+                                foreignField:"_id",
+                                as : "owner",
+                                pipeline:[
+                                    {
+                                        $project:{
+                                            username:1,
+                                            avator:1,
+                                            createdAt:1,
+                                            updatedAt:1
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            $addFields:{
+                                owner:{
+                                    $first:"$owner"
+                                }
+                            }
+                        }
+                    ],
+                    matchedCount:[
+                        {$count:"$videos"}
+                    ]
+                }
+            }
+        ])
+    
+        if(!getVideos[0].videos?.length){
+            throw new ApiError(400,null, "Page not found")
+        }
+        if(!getVideos[0].matchedCount?length){
+            throw new ApiError(400,null,"No videos found")
+        }
+    
+        return res.status(200)
+        .json(new ApiResponse(200,getVideos[0].videos,"Videos fetched successfully!"))
+    } catch (error) {
+        throw new ApiError(500, "An error occurred while fetching videos")
+    }
+
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
+    const { title, description , videoFile , thumbnail} = req.body
     // TODO: get video, upload to cloudinary, create video
+    
+    const owner = await User.findById(req.user._id)
+    if(!owner){
+        throw new ApiError(400, "User not found")
+    }
+
+    if(!videoFile || !thumbnail || !title || !description){
+        throw new ApiError(400, "Please provide all required details")
+    }
+
+    const uploadedVideo = await uploadOnCloudinary(videoFile)
+    const uploadedThumbnail = await uploadOnCloudinary(thumbnail)
+
+    if(!uploadedThumbnail || !uploadedVideo){
+        throw new ApiError(500, "Failed to upload the video!")
+    }
+
+    const newVideo = await Video.create({
+        title,
+        description,
+        videoFile:uploadedVideo.secure_url,
+        thumbnail:uploadedThumbnail.secure_url,
+        duration:uploadedVideo.duration,
+        isPublised:false,
+        owner:owner._id
+    })
+    if(!newVideo){
+        throw new ApiError(500, "An error occured while uploading the video")
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200, newVideo,"Video uploaded successfully!"))
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(400, "Video not found")
+    }
+
+    if(!video.isPublised){
+        throw new ApiError(400, "Video is not published")
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200, "Video fetched successfully!"))
+
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"Invalid video id")
+    }
+    const {title , description, thumbnail} = req.body
+    const updatedVideo = await Video.findByIdAndUpdate(videoId,{
+        title,
+        description,
+        thumbnail,
+    },
+    {
+        new:true
+    })
+    if(!updatedVideo){
+        throw new ApiError(500, "An error occurred while updating the video")
+    }
 
+    return res.status(200)
+    .json(new ApiResponse(200, updatedVideo,"Video updated Successfully!"))
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video id")
+    }
+
+    if(req.user._id!==video.owner.toString()){
+        throw new ApiError(400, "Unathuorized!")
+    }
+    const deletedVideo = await Video.findByIdAndDelete(videoId)
+    if(!deletedVideo){
+        throw new ApiError(500, "An error occurred while deleting the video!")
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200, "video deleted successfully!"))
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    if(isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video Id")
+    }
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(400, "Video Not found!")
+    }
+    if(req.user._id !== video.owner.toString()){
+        throw new ApiError(400, "Unauthorized!")
+    }
+    if(video.isPublised){
+        await Video.findByIdAndUpdate(videoId,{isPublised:false})
+        return res.status(200)
+        .json(new ApiResponse(200, "video unpublished successfully"))
+    }else{
+        await Video.findByIdAndUpdate(videoId,{isPublised:true})
+
+        return res.status(200)
+        .json(new ApiResponse(200, "Video published successfully!"))
+    }
 })
 
 export {
